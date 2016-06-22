a7ab999ee718008bb2d16ee70f8f7dc718f2a6ca
James J Balamuta <james.balamuta@gmail.com>
2016-06-22 15:07:28 -0500

    init package

---
+++ R/grader.R
@@ -0,0 +1,5 @@
+#' Create Homework Assignments, Solutions, and Rubrics using rmarkdown
+#'
+#' Grader enables the unibody design of homework solutions using rmarkdown.
+#' @importFrom rmarkdown render
+"_PACKAGE"
